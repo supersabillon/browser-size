abf0cb18f29bf31be4fdb0b352caaaabd3ee9505
Joel Sabillon <js1894@yp.com>
2016-01-05 13:01:50 -0800

    adding zeroclipboard config

--- scripts.js
+++ scripts.js
@@ -11,8 +11,27 @@ $(window).resize(function() {
 $(function() {
     browserSize();
 
-    $('.copy').click(function(event) {
-    	event.preventDefault();
-    	console.log('target is ' + $(this).prev().text());
-	});
+    // $('.copy').click(function(event) {
+    // 	//event.preventDefault();
+    // 	//console.log('target is ' + $(this).prev().text());
+	   // });
+
+    var client = new ZeroClipboard($('.copy'));
+
+    client.on( 'ready', function(event) {
+      console.log( 'movie is loaded' );
+
+      client.on( 'copy', function(event) {
+        event.clipboardData.setData('text/plain', event.target.innerHTML);
+      } );
+
+      client.on( 'aftercopy', function(event) {
+        console.log('Copied text to clipboard: ' + event.data['text/plain']);
+      } );
+    } );
+
+    client.on( 'error', function(event) {
+      console.log( 'ZeroClipboard error of type "' + event.name + '": ' + event.message );
+      ZeroClipboard.destroy();
+    } );
 });
