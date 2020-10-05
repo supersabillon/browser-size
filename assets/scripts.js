10cb663e73c90320241f989c51c4a7b1036b56f9
Joel Sabillon <supersabillon@users.noreply.github.com>
2020-10-05 10:11:49 -0700

    A11y improvements (#4) (#5)
    Co-authored-by: Joel Sabillon <joel.sabillon@hulu.com>
    
    Co-authored-by: Joel Sabillon <joel.sabillon@hulu.com>

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,33 +1,51 @@
-$(function() {
-  'use strict';
+$(function () {
+  "use strict";
 
   var $win = $(window),
-    $width = $('#width').text($win.width()),
-    $height = $('#height').text($win.height());
+    $width = $("#width").text($win.width()),
+    $height = $("#height").text($win.height());
 
-  $win.resize(function() {
-    $width.text($win.width()),
-    $height.text($win.height());
-  });
-
-  $('.copy').each(function() {
-    var clipboard = new Clipboard(this);
-
-    clipboard.on('success', function(e) {      
-      e.clearSelection();
-      $(".js-alert").fadeIn(500, function(){
-        $(this).fadeOut(1000);
-      });
-    });
+  $win.resize(function () {
+    $width.text($win.width()), $height.text($win.height());
   });
 });
 
+function copyToClipboard(type) {
+  const sizeText = document.getElementById(type);
+  const selection = window.getSelection();
+  const range = document.createRange();
+
+  // Select the text
+  range.selectNodeContents(sizeText);
+
+  selection.removeAllRanges();
+  selection.addRange(range);
+
+  // Copy the text
+  document.execCommand("copy");
+  selection.removeAllRanges();
+
+  $(".js-alert").fadeIn(300, function () {
+    setTimeout(() => {
+      $(this).fadeOut(2000);
+    }, 500);
+  });
+}
 
 //analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+(function (i, s, o, g, r, a, m) {
+  i["GoogleAnalyticsObject"] = r;
+  (i[r] =
+    i[r] ||
+    function () {
+      (i[r].q = i[r].q || []).push(arguments);
+    }),
+    (i[r].l = 1 * new Date());
+  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m);
+})(window, document, "script", "//www.google-analytics.com/analytics.js", "ga");
 
-ga('create', 'UA-70117330-2', 'auto');
-ga('send', 'pageview');
+ga("create", "UA-70117330-2", "auto");
+ga("send", "pageview");
